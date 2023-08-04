f8363f8fb8adaf2fb444518c1bc83c77ac346133
Mezzky <ryansahadha@gmail.com>
2023-08-04 15:02:53 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    screens: {
+      'sm': '320px',
+      'md': '375px', 
+      'lg': '425px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
+    backgroundImage: {
+      'abu': "url('../Assets/background/bg-bercak.png')"
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', 'sans-serif'",
+        outfit: "'Outfit', 'sans-serif'",
+        quicksand: "'Quicksand', 'sans-serif'"
+      },
+      colors: {
+        main: '#CB9D4D',
+        dark: '#222222',
+        text: '#A0A0A0',
+        bg1: '#E4E4E4',
+        bg2: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
