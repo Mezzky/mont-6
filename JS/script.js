f2e3ed698ec8c9ef22108e2e9d79fa577aad2175
Mezzky <ryansahadha@gmail.com>
2023-08-06 00:08:22 +0800

    Done Landing Page all Devices

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+// Variables
+const hamburger = document.getElementById('hamburger');
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("header .navbar");
+const header = document.querySelector('header');
+const year = document.getElementById('year');
+
+
+// Scrolled Animation
+// window.addEventListener('scroll', () => {
+//     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         header.classList.add('scrolled');
+//         nav.classList.add('scrolled');
+//     } else{
+//         header.classList.remove('scrolled');
+//         nav.classList.remove('scrolled');
+//     }
+// });
+
+// Hamburger
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+});
+
+// Menu Toggle
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('slide');
+});
+
+// Copyright
+let date = new Date().getFullYear();
+year.innerHTML = date;
